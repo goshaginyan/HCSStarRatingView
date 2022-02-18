f8a4246d6351db57f83d42a4b6a31428b8430d5d
George <darkglaz13@gmail.com>
2022-02-18 09:09:20 +0530

    Added SPM

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.1
+ import PackageDescription
+
+ let package = Package(
+     name: "HCSStarRatingView",
+     products: [
+         .library(name: "HCSStarRatingView", targets: ["HCSStarRatingView"])
+     ],
+     targets: [
+         .target(
+             name: "HCSStarRatingView",
+             path: "HCSStarRatingView",
+             exclude: ["Info.plist"],
+             publicHeadersPath: "",
+             cSettings: [
+                 .headerSearchPath("")
+             ]
+         )
+     ]
+ )
